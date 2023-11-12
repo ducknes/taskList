42e211a3ee073b659300c635f7756346886ceb85
Ilya Antonov <i.antonov@smartway.today>
2023-11-13 01:12:00 +0300

    docker-compose & db fix

--- database/postgres.go
+++ database/postgres.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
@@ -10,6 +12,14 @@ func NewPostgresConnection(connectionString string) (*sqlx.DB, error) {
 	db, err := sqlx.Connect("postgres", connectionString)
 	if err != nil {
 		return nil, fmt.Errorf("postgres: can`t connect to database. err: %v", err)
+	}
+
+	for i := 0; i < 5; i++ {
+		time.Sleep(30 * time.Microsecond)
+		if err = db.Ping(); err != nil {
+			continue
+		}
+		break
 	}
 
 	if err = db.Ping(); err != nil {
